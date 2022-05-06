65c8717d1903c90abc6007b08ddbb47fa552f650
“S3lk1rk” <matthew.selkirk51@gmail.com>
2022-05-06 18:35:53 +0100

    cleanup

--- models/userModel.js
+++ models/userModel.js
@@ -16,15 +16,6 @@ class UserDAO {
     // for the demo the password is the bcrypt of the user name
     init() {
         this.db.insert({
-            user: 'Peter',
-            password:
-            '$2b$10$I82WRFuGghOMjtu3LLZW9OAMrmYOlMZjEEkh.vx.K2MM05iu5hY2C'
-        });
-        this.db.insert({
-            user: 'Ann',
-            password: '$2b$10$bnEYkqZM.MhEF/LycycymOeVwkQONq8kuAUGx6G5tF9UtUcaYDs3S'
-        });
-        this.db.insert({
             user: 'Staff',
             password: '$2a$10$bDd73tCG2cIGTLjWG30uqu8gk89Ko6bJjMkPNaIzH7xZ6Q6W1ZVE.'
         });
